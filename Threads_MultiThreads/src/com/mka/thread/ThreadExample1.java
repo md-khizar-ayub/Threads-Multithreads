9cf9f2e4c83d40d740a746ced38eb0eadb2cbeaf
khizar <mohammedkhizar501@gmail.com>
2024-06-23 13:32:06 +0530

    Initialized a threads name.

--- Threads_MultiThreads/src/com/mka/thread/ThreadExample1.java
+++ Threads_MultiThreads/src/com/mka/thread/ThreadExample1.java
@@ -4,6 +4,7 @@ public class ThreadExample1 extends Thread {
 
 	@Override
 	public void run() {
+		Thread.currentThread().setName("Thread 111");
 		for(int i=0;i<=10;i++) {
 			System.out.println(Thread.currentThread().getName() +" - "+i );
 		}
--- Threads_MultiThreads/src/com/mka/thread/ThreadExample2.java
+++ Threads_MultiThreads/src/com/mka/thread/ThreadExample2.java
@@ -4,6 +4,7 @@ public class ThreadExample2 implements Runnable{
 
 	@Override
 	public void run() {
+		Thread.currentThread().setName("Thread 222");
 		for(int i=0;i<=10;i++) {
 			System.out.println(Thread.currentThread().getName() +" - "+i );
 		}
